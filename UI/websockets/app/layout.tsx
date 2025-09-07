0eebe45cc11186cffe1e3907ecfd85b171418ec4
Oliver Sheldon <oliver-sheldon@hotmail.co.uk>
2025-09-07 14:50:35 +0100

    init ui

---
+++ UI/websockets/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Viewport } from 'next'
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1.0
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+      </head>
+      <body>
+        <div id="wrapper">
+        </div>
+      </body>
+    </html>
+  )
+}
